f8b1dc6b81e1a573906cc83a79f01bd45e584ad1
greyu <gre_yu@163.com>
2018-03-08 01:32:42 -0800

    update navbar, add modify password, signout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {
   Layout,
-  Icon
+  Icon,
+  Menu,
+  Dropdown
 } from 'antd';
 const { Header } = Layout;
 
@@ -11,7 +13,23 @@ export default class Navbar extends React.Component {
     collapsed: PropTypes.bool.isRequired,
     handleClick: PropTypes.func.isRequired
   }
+
+  renderOverlay() {
+    return (
+      <Menu>
+        <Menu.Item>
+          <a href="">修改密码</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a href="">退出</a>
+        </Menu.Item>
+      </Menu>
+    )
+  }
+
   render() {
+    const menu = this.renderOverlay()
+
     return (
       <Header>
         <nav className="navbar">
@@ -23,6 +41,17 @@ export default class Navbar extends React.Component {
                 onClick={this.props.handleClick}
               />
             </li>
+          </ul>
+          <ul className="nav navbar-right">
+            <Dropdown
+              overlay={menu}
+            >
+              <li className="nav-item">
+                <Icon
+                  type="user"
+                />
+              </li>
+            </Dropdown>
           </ul>
         </nav>
       </Header>
