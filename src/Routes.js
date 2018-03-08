0ce640d72b872fbb9a4e845067bbb2ff9004b3e0
greyu <gre_yu@163.com>
2018-03-07 19:15:37 -0800

    get user from localStorage when website start

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
+import { connect } from 'react-redux';
 import PrivateRoute from './containers/PrivateRoute';
 import Login from './containers/Login';
 import Home from './containers/Home';
+import * as storage from './utils/storage';
+import {
+  USER_ID,
+  TOKEN
+} from './constants';
+import {
+  setCurrentUser
+} from './actions'
 
+@connect(
+  state => {
+    return {
+      isAuthenticated: state.auth.isAuthenticated
+    }
+  },
+  dispatch => ({
+    setCurrentUser: (user) => {
+      return dispatch(setCurrentUser(user))
+    }
+  })
+)
 export default class Routes extends React.Component {
+  static propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    setCUrrentUser: PropTypes.func.isRequired
+  }
+
+  componentWillMount() {
+    const userId = storage.getStorage(USER_ID)
+    const token = storage.getStorage(TOKEN)
+
+    if (userId && token) {
+      this.props.setCurrentUser({
+        userId,
+        token
+      })
+    }
+  }
+
   render() {
     return (
       <Router>
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="navbar-header">
+        </div>
+        <div className="navbar-container">
+        </div>
+      </nav>
+    )
+  }
+}
